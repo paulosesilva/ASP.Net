0cbaf9e066afb0878dafaff4e262596148868cf4
Jefferson <jeffersondossantos1995@hotmail.com>
2018-07-27 12:55:21 -0300

    Adicionando Categorias e foreach para exibir os produtos cadastrados

---
+++ ECommerce/ECommerce/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaID { get; set; }
+        public string Descricao { get; set; }
+    }
+}
